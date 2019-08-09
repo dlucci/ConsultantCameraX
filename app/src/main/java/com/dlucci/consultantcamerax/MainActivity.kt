7c40edb92a774e94e5204ef4b174b977deaa8192
Derril Lucci <derril.lucci@gmail.com>
2019-08-09 11:06:48 -0400

    -Migrating to Android X -Improving how we do dependencies in application gradle file -Requesting camera permission -Adding material design dependency

--- app/src/main/java/com/dlucci/consultantcamerax/MainActivity.kt
+++ app/src/main/java/com/dlucci/consultantcamerax/MainActivity.kt
@@ -1,12 +1,47 @@
 package com.dlucci.consultantcamerax
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.TextureView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+private const val REQUEST_CODE_PERMISSIONS = 42
+
+private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+
+class MainActivity : AppCompatActivity(), LifecycleOwner {
+
+    private lateinit var viewFinder : TextureView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun startCamera() {
+        //TODO add this function
+    }
+
+    private fun updateTransform() {
+        //TODO add this function
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if(requestCode == REQUEST_CODE_PERMISSIONS) {
+            if(allPermissionsGranted()){
+                viewFinder.post { startCamera() }
+            } else {
+                Snackbar.make(view_finder, "You did not grant all permissions", Snackbar.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
     }
 }
